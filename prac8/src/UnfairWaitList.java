29592aa4723fb36e092b24e05079e5ada1535a7e
Дмитрий Лисютин <54859565+SelectionForCollection@users.noreply.github.com>
2021-11-11 12:31:26 +0300

    Update UnfairWaitList.java

--- prac8/src/UnfairWaitList.java
+++ prac8/src/UnfairWaitList.java
@@ -2,15 +2,42 @@ import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class UnfairWaitList<T> extends WaitList {
-    UnfairWaitList(){
+    protected ConcurrentLinkedQueue<T> deletedElements;
 
-    };
-    public void remove(T element){
-        content.remove(element);
+    UnfairWaitList(){
     }
+
+    public void remove(T element){
+        if(content.peek() != element){
+            content.remove(element);
+        }else{
+            System.out.println("Первый элемент удалить нельзя");
+        }
+    }
+
     public void moveToBack(T element){
         if(this.content.remove(element)) {
             this.content.add(element);
         }
+    }
+
+    @Override
+    public void add(Object element) {
+        if(deletedElements != null){
+            if(!deletedElements.contains(element)){
+                super.add(element);
+            }else{
+                super.add(element);
+                deletedElements.remove(element);
+            }
+        }else{
+            super.add(element);
+        }
+
+    }
+
+    public String toString(){
+        System.out.print("Список - ");
+        return content.toString();
     }
 }
