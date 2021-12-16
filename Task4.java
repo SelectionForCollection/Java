dd113f787ed949d9fd87f315731c0f363c414fc8
Дмитрий Лисютин <54859565+SelectionForCollection@users.noreply.github.com>
2021-12-16 12:37:28 +0300

    Delete Task4.java

--- Task4.java
+++
@@ -1,7 +0,0 @@
-import java.util.regex.Pattern;
-
-public class Task4 {
-    public static void main(String[] args) {
-        System.out.println(Pattern.compile("\\d+\\s*\\+").matcher(" (1 - 8) – 9 / 4 +").find());
-    }
-}
